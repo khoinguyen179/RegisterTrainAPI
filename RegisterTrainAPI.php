0d550e1c34a66e8dc7e6992b2cf09e44e7d1d7ed
root <khoinguyengh69@gmail.com>
2025-05-05 06:02:18 +0100

    update commit module

--- RegisterTrainAPI.php
+++ RegisterTrainAPI.php
@@ -1,6 +1,31 @@
 <?php
 
-
 if(!defined("WHMCS")){
     die("This file cannot be accessed directly");
+}
+
+use WHMCS\Database\Capsule;
+function RegisterTrainAPI_config()
+{
+    return[
+        'name' => 'Vietnix Register Train API Module',
+        'description' => 'Register Module for Customer us e API',
+        'version' => '1.0',
+        'author' => 'Vietnix',
+    ];
+}
+
+function RegisterTrainAPI_activate(){
+    try {
+        Capsule::schema()->create("tblTokens", function ($table) {
+            $table->increments("id");
+            $table->string("token");
+        });
+    }
+    catch (Exception $e){
+        return [
+            'status' => 'error',
+            'message' => 'Không thể tạo bảng'
+        ];
+    }
 }
